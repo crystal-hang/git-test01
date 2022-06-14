da00c183f75b0a57187df8e9e6a3b769c2feb9f7
LiHang <1097786158@qq.com>
2022-06-14 16:47:19 +0800

    third-commit

--- src/main/java/com/lihang/git/GitTest.java
+++ src/main/java/com/lihang/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello idea-git!");
         System.out.println("hello idea-git1!");
+        System.out.println("hello idea-git2!");
     }
 }
