5be4345ba1e5e81e33055168d119d74b27fccf8f
LiHang <1097786158@qq.com>
2022-06-14 16:46:07 +0800

    second-commit

--- src/main/java/com/lihang/git/GitTest.java
+++ src/main/java/com/lihang/git/GitTest.java
@@ -3,5 +3,6 @@ package com.lihang.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello idea-git!");
+        System.out.println("hello idea-git1!");
     }
 }
